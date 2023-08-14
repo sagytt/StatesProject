14c5d5654157d5cf6eaae6e4bd1576d3823c5866
Sagi <sagix7004@gmail.com>
2023-08-14 14:15:33 +0300

    created seeder for admin user

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware' => ['guest']], function () {
+    Route::post('/', [AuthenticatedSessionController::class, 'create'])->name('login');
+    Route::get('register', [AuthenticatedSessionController::class, 'register_view'])->name('register');
 });
+
+
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('home', [AuthenticatedSessionController::class, 'home'])->name('home');
+    Route::get('logout', [AuthenticatedSessionController::class, 'logout'])->name('logout');
+    Route::post('insert-state', [StateController::class, 'insert_state'])->name('insert');
+    Route::get('/home', [StateController::class, 'Showdata'])->name('home');
+    Route::get('delete/{id}', [StateController::class, 'delete_state']);
+    Route::get('edit/{id}', [StateController::class, 'edit_state']);
+    Route::post('update/{id}', [StateController::class, 'update_state']);
+});
+
